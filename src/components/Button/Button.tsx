f52847725610f461ad443539b995df2bd0871c97
flexwappa <flexwappa@ya.ru>
2021-05-24 19:51:21 +0300

    create component Button

---
+++ src/components/Button/Button.tsx
@@ -0,0 +1,24 @@
+import * as React from "react";
+import "./Button.styles.css"
+
+type PropsType = {
+    text: string
+    loading: boolean
+    disabled: boolean
+    onClick: () => void
+}
+
+export const Button  = ( {text, loading, disabled, onClick}: PropsType) => {
+
+    const onClickHandler = () => {
+        if (loading || disabled) return
+        onClick()
+    }
+
+    return (
+        <div className={`button ${disabled && "button-disabled"}`} onClick={onClickHandler}>
+            {text}
+        </div>
+    )
+}
+
