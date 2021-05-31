f2d30f9c813aebee1a2a6cc6f3a78bf0ebe05b12
flexwappa <flexwappa@ya.ru>
2021-05-31 06:47:31 +0300

    add Dropdown List

--- src/components/Tabs/Tab/Tab.tsx
+++ src/components/Tabs/Tab/Tab.tsx
@@ -1,44 +1,105 @@
-import React, {useState} from "react";
+import React, {createRef, DetailedHTMLProps, HTMLAttributes, useEffect, useRef, useState} from "react";
 import './Tab.styles.css'
 
 
 type PropsTypes = {
     label: string
     onClick: Function
+    onChangeDropDown: Function
+    executeClick: Function | any
+    removeRequest: Function
     isActive: boolean
-    isSuccess: boolean
+    isLast: boolean
+    error: boolean
+    inputText: string
 }
 
-export const Tab = React.memo(
-    ({label, isActive, onClick, isSuccess}: PropsTypes) => {
+export const Tab =
+    ({label, isLast, isActive, onClick, executeClick, removeRequest, error,inputText}: PropsTypes) => {
 
-        const [optionsMode, setOptionsMode] = useState(false)
+        const [dropDown, setDropDown] = useState(false)
         const [copyMode, setCopyMode] = useState(false)
-
+        const dropDownStateRef = useRef<any>();
+        dropDownStateRef.current = dropDown
+        console.log("ERRRRORRR", error)
         const onCopyHandler = () => {
+            console.log('onCopyHandler')
+            setCopyMode(!copyMode)
+            setDropDown(false)
+            //Copy to clipboard
+            navigator.clipboard.writeText(inputText).then(function() {
+                console.log('Async: Copying to clipboard was successful!');
+            }, function(err) {
+                console.error('Async: Could not copy text: ', err);
+            });
+        }
+        const changeDropDown = (event:React.MouseEvent<any>, boolean: boolean) => {
+            event.stopPropagation()
+            console.log("DROP DOWN")
+            // onChangeDropDown(boolean)
+            setDropDown(boolean)
+        }
 
+        const tab = useRef<any>()
+
+        const onClickAnotherPlaceHandler = (event: MouseEvent) => {
+            const isIncludesTab = event.composedPath().includes(tab.current)
+            dropDownStateRef.current && !isIncludesTab && setDropDown(false)
+        }
+
+        useEffect(() => {
+            document.addEventListener('click', onClickAnotherPlaceHandler)
+            return () => {
+                document.removeEventListener("click", onClickAnotherPlaceHandler)
+            }
+        }, [])
+
+
+        const onClickRemoveHandler = () => {
+            removeRequest()
+            setDropDown(false)
+        }
+        const onClickExecuteHandler = () => {
+            removeRequest()
+            executeClick()
         }
 
         return (
-            <div className={`tab ${isActive && "tab-active"}`}
-                 onClick={() => onClick()}>
-                <img className='tab__request-feedback'
-                     src={`${isSuccess
-                         ? '/icons/green-dot.svg'
-                         : '/icons/red-dot.svg'}`}/>
-                <span className="tab__label">{label}</span>
-                <img className='tab__options-button' src='/icons/dots.svg' onClick={() => setOptionsMode(!optionsMode)}/>
-                { optionsMode && <div className='tab__options-list'>
-                    <div>Выполнить</div>
-                    <div onClick={ () => setCopyMode(!copyMode) }>Скопировать</div>
-                    <div>Удалить</div>
-                </div> }
-                { copyMode && <div className='tab__copied'>
-                    <span>
-                        Скопировано
-                    </span>
-                </div> }
+            <div
+                className={"tab-wrapper"}
+                ref={tab}
+            >
+                <div onClick={() => onClick()}  className={`tab ${isActive && "tab-active"}`}>
+                    <img className='tab__request-feedback'
+                         src={
+                             `${error
+                                 ? '/icons/red-dot.svg'
+                                 : '/icons/green-dot.svg'}`
+                         }
+                    />
+                    <span className="tab__label">{label}</span>
+                    <img
+                        className='tab__options-button'
+                        src='/icons/dots.svg'
+                        onClick={(event) => changeDropDown(event, !dropDown)}
+                    />
+                </div>
+                {dropDown && <div className='tab__options-list'>
+                    <div onClick={ onClickExecuteHandler }>Выполнить</div>
+                    <div onClick={ onCopyHandler }>Скопировать</div>
+                    {!isLast && <div onClick={onClickRemoveHandler} className='tab__options-list__delete'>Удалить</div>}
+                </div>}
+                {
+                    copyMode &&
+                    <div className={`tab__copied`}>
+                        <span className='tab__copied__span'>
+                            Скопировано
+                        </span>
+
+                    </div>
+
+                }
             </div>
+
         )
     }
-)
