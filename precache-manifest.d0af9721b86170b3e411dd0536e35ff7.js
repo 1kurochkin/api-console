c2df05cbc2927ddd21dcefe85701daca93c4366b
payting-app <paytingApp@gmail.com>
2021-05-31 08:06:58 +0300

    Updates

---
+++ precache-manifest.d0af9721b86170b3e411dd0536e35ff7.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e3d55345e372c63cbeb2f6647d0d0e83",
+    "url": "/api-console/index.html"
+  },
+  {
+    "revision": "fbb615266aa41104d646",
+    "url": "/api-console/static/css/main.ee90552b.chunk.css"
+  },
+  {
+    "revision": "4ebae824d1c6fd91260e",
+    "url": "/api-console/static/js/2.efd88e98.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/api-console/static/js/2.efd88e98.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "fbb615266aa41104d646",
+    "url": "/api-console/static/js/main.2c8d31ca.chunk.js"
+  },
+  {
+    "revision": "4d20d334907fa35e0959",
+    "url": "/api-console/static/js/runtime-main.5793e4b0.js"
+  }
+]);
