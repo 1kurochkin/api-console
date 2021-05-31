775983e6f26f72c3c1a60eea59a5b99244c7dca4
flexwappa <flexwappa@ya.ru>
2021-05-31 06:47:25 +0300

    add Controller to Tab View inputs

--- src/components/Tabs/Tab/TabView/TabView.tsx
+++ src/components/Tabs/Tab/TabView/TabView.tsx
@@ -1,27 +1,44 @@
 import React from "react";
 import './TabView.styles.css'
+import {Controller} from "react-hook-form";
+import {FORM} from "../../../../constants";
 
 
 type PropsTypes = {
-    text: string
-    error: boolean
+    error: boolean,
+    inputField: any,
+    outputField: any
+    control: any
 }
 
-export const TabView = React.memo(
-    ({ text, error }: PropsTypes) => {
+export const TabView = ({error, control, inputField, outputField}: PropsTypes) => {
 
-        return (
-            <div className={`tab-view ${error && "tab-view-error"}`}>
+    console.log('outputField ', !!error, outputField)
 
-                <div className="tab-view__input-field">
-                    <span className="tab-view__input-field__span">Запрос:</span>
-                    <input className="tab-view__input-field__input" value={text}/>
-                </div>
-                <div className="tab-view__input-field">
-                    <span className="tab-view__input-field__span">Ответ:</span>
-                    <input className="tab-view__input-field__input"/>
-                </div>
+    return (
+        <div className={`tab-view ${!!error && "tab-view-error"}`}>
+            <div className="tab-view__input-field">
+                <span className="tab-view__input-field__span">Запрос:</span>
+                <Controller
+                    control={control}
+                    name={FORM.TEXT_AREA_INPUT.name}
+                    render={() => <textarea
+                        {...inputField}
+                        className="tab-view__input-field__textarea"/>
+                    }
+                />
             </div>
-        )
-    }
-)
+            <div className="tab-view__input-field">
+                <span className="tab-view__input-field__span">Ответ:</span>
+                <Controller
+                    control={control}
+                    name={FORM.TEXT_AREA_OUTPUT.name}
+                    render={() => <textarea value={ error ? error : outputField.value }
+                                            className="tab-view__input-field__textarea"/>
+                    }
+                />
+
+            </div>
+        </div>
+    )
+}
